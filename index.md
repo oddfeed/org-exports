77768392d42122720dcd43c62019b5cc8f2ab0a9
oddfeed <144010979+oddfeed@users.noreply.github.com>
2024-03-01 16:05:31 +0000

    Fixed reverse

--- index.md
+++ index.md
@@ -2,18 +2,13 @@
 <script async src="https://analytics.goinghome.earth/script.js" data-website-id="519b085c-73db-408b-bf40-6cb4d158dfbc"></script>
 </head>
 # Masterlist
-This index was automatically generated
+This index was automatically generated and is sorted chronologically based on updated time
 ---
 # 2024
-<<<<<<< HEAD
-## February
-=======
 ## March
 - [202402-driving.html](https://org.alienate.earth/202402-driving.html)
->>>>>>> 8d351db0fb03895e3479c6ec67ce92c1a18751e3
+
+## February
 - [logs.html](https://org.alienate.earth/logs.html)
 - [reading.html](https://org.alienate.earth/reading.html)
-
-## March
-- [202402-driving.html](https://org.alienate.earth/202402-driving.html)
 
--- indexer.sh
+++ indexer.sh
@@ -6,7 +6,7 @@ echo "<head>" >> "$output_file"
 echo '<script async src="https://analytics.goinghome.earth/script.js" data-website-id="519b085c-73db-408b-bf40-6cb4d158dfbc"></script>' >> "$output_file"
 echo "</head>" >> "$output_file"
 echo "# Masterlist"  >> "$output_file"
-echo "This index was automatically generated"  >> "$output_file"
+echo "This index was automatically generated and is sorted chronologically based on updated time"  >> "$output_file"
 echo "---" >> "$output_file"
 
 declare -A files_by_year_month
@@ -23,7 +23,7 @@ done
 
 # Sort years and months and write to the file
 previous_year=""
-for year_month in $(echo ${!files_by_year_month[@]} | tr ' ' '\n' | sort -u); do
+for year_month in $(echo ${!files_by_year_month[@]} | tr ' ' '\n' | sort -ur); do
     year=${year_month:0:4}
     month=${year_month:5:2}
 
