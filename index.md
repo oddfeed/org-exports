fa173565e6e9a26ae1ca84ec52a43fc1ad967be0
oddfeed <oddfeed@pm.me>
2024-05-11 14:11:48 +0100

    new script

--- index.md
+++ index.md
@@ -2,10 +2,9 @@
 <script async src="https://analytics.goinghome.earth/script.js" data-website-id="519b085c-73db-408b-bf40-6cb4d158dfbc"></script>
 </head>
 # Masterlist
-This index was automatically generated and is sorted chronologically based on updated time
+This index was automatically generated and is sorted by year created
 ---
-# 2019
-## 
+# 2024
 - [202402-driving.html](https://org.alienate.earth/202402-driving.html)
 - [logs.html](https://org.alienate.earth/logs.html)
 - [reading.html](https://org.alienate.earth/reading.html)
--- indexer.sh
+++ indexer.sh
@@ -6,42 +6,31 @@ echo "<head>" >> "$output_file"
 echo '<script async src="https://analytics.goinghome.earth/script.js" data-website-id="519b085c-73db-408b-bf40-6cb4d158dfbc"></script>' >> "$output_file"
 echo "</head>" >> "$output_file"
 echo "# Masterlist"  >> "$output_file"
-echo "This index was automatically generated and is sorted chronologically based on updated time"  >> "$output_file"
+echo "This index was automatically generated and is sorted by year created"  >> "$output_file"
 echo "---" >> "$output_file"
 
-declare -A files_by_year_month
+files_by_year=()
 
 for file in *.{html,md,pdf}; do
     if [ "$file" == "README.md" ] || [ "$file" == "index.md" ]; then
         continue
     fi
 
-    year_month=$(date -r "$file" +"%Y-%m")
+    year=$(stat -f "%Sm" -t "%Y" "$file")
+
+    if [[ ! ${files_by_year[$year]+_} ]]; then
+        files_by_year[$year]=""
+    fi
 
     if [[ "$file" == *.pdf ]]; then
-        files_by_year_month[$year_month]+="- [$file](https://org.alienate.earth/${file%.*}.pdf)\n"
+        files_by_year[$year]+="- [$file](https://org.alienate.earth/${file%.*}.pdf)\n"
     else
-        files_by_year_month[$year_month]+="- [$file](https://org.alienate.earth/${file%.*}.html)\n"
+        files_by_year[$year]+="- [$file](https://org.alienate.earth/${file%.*}.html)\n"
     fi
-    
 done
 
-# Sort years and months and write to the file
-previous_year=""
-for year_month in $(echo ${!files_by_year_month[@]} | tr ' ' '\n' | sort -ur); do
-    year=${year_month:0:4}
-    month=${year_month:5:2}
-
-    # Check if the year has changed and add a year heading if it has
-    if [ "$year" != "$previous_year" ]; then
-        echo "# $year" >> "$output_file"
-        previous_year=$year
-    fi
-
-    # Convert month number to month name
-    month_name=$(date -d "$year-$month-01" +"%B")
-
-    echo "## $month_name" >> "$output_file"
-    echo -e "${files_by_year_month[$year_month]}" >> "$output_file"
+# Write to the file
+for year in "${!files_by_year[@]}"; do
+    echo "# $year" >> "$output_file"
+    echo -e "${files_by_year[$year]}" >> "$output_file"
 done
-
